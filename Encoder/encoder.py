4732513abb7d496f6052282574d999cf49ebcffa
dyz <32426751+DongyaoZhu@users.noreply.github.com>
2020-05-17 19:42:55 -0700

    fixing generate

--- Encoder/encoder.py
+++ Encoder/encoder.py
@@ -16,11 +16,16 @@ class Encoder():
     tkl = tf.keras.layers
     filters = 368
     for i in range(6):
+      old = x
       x = tkl.Conv1D(filters=2*filters,
                kernel_size=4, 
                strides=2,
                padding='same')(x)
       x = tf.nn.tanh(x[:, :, : filters]) * tf.nn.sigmoid(x[:, :, filters: ])
+      x += tkl.Conv1D(filters=filters,
+                      kernel_size=1,
+                      strides=2,
+                      padding='valid')(old)
     x = tkl.Conv1D(filters=self.latent_dim,
              kernel_size=1, 
              strides=1, 
@@ -35,38 +40,59 @@ class Encoder_():
     super(Encoder_, self).__init__()
     self.scope = scope
     self.latent_dim = latent_dim
-    self.trainable = trainable
     self.layers = []
+    tkl = tf.keras.layers
     encoder_layers = [(2, 4, 1),
-              (2, 4, 1),
-              (2, 4, 1),
-              (1, 4, 1),
-              (2, 4, 1),
-              (1, 4, 1),
-              (2, 4, 1),
-              (1, 4, 1),
-              (2, 4, 1),
-              (1, 4, 1)]
+                      (2, 4, 1),
+                      (2, 4, 1),
+                      (1, 4, 1),
+                      (2, 4, 1),
+                      (1, 4, 1),
+                      (2, 4, 1),
+                      (1, 4, 1),
+                      (2, 4, 1),
+                      (1, 4, 1)]
     for strides, kernel_size, dilations in encoder_layers:
       conv_wide = tkl.Conv1D(filters=2*latent_dim,
                  kernel_size=kernel_size, 
                  strides=strides,
+                 padding='valid',
+                 trainable=trainable)
+      conv_1x1 = tkl.Conv1D(filters=latent_dim,
+                 kernel_size=1, 
+                 strides=1,
                  padding='same',
-                 activation='relu')
-      conv_1x1 = 0
-      self.layers.append((conv_wide, conv_1x1))
-    self.post1 = 0
-    self.post2 = 0
+                 trainable=trainable)
+      skip = (kernel_size - strides) * dilations
+      self.layers.append((conv_wide, conv_1x1, skip, strides))
+    self.post1 = tkl.Conv1D(filters=latent_dim,
+                 kernel_size=1, 
+                 strides=1,
+                 padding='same',
+                 trainable=trainable)
+    self.post2 = tkl.Conv1D(filters=latent_dim,
+                 kernel_size=1, 
+                 strides=1,
+                 padding='same',
+                 trainable=trainable)
 
   def __call__(self, x):
-    for conv_wide, conv_1x1, skip, strides in self.layers:
-      x = conv_wide(x, trainable=trainable)
-      x = tf.nn.tanh(x[:, :, : self.latent_dim]) * tf.nn.sigmoid(x[:, :, self.latent_dim: ])
-      res = conv_1x1(x, trainable=trainable)
-      x = res + x
-    x = self.post1(x, trainable=trainable)
-    x = tf.nn.relu(x)
-    x = self.post2(x, trainable=trainable)
+    with tf.variable_scope(self.scope):
+      for i, (conv_wide, conv_1x1, skip, strides) in enumerate(self.layers):
+        old = x
+        x = conv_wide(x)
+        x = tf.nn.tanh(x[:, :, : self.latent_dim]) * tf.nn.sigmoid(x[:, :, self.latent_dim: ])
+        res = conv_1x1(x)
+        if i == 0:
+          x = res
+        else:
+          old = old[:, skip: skip+tf.shape(res)[1]*strides, :]
+          old = tf.reshape(old, [tf.shape(old)[0], tf.shape(res)[1], -1, tf.shape(res)[2]])
+          old = old[:, :, -1, :]
+          x = res + old
+      x = self.post1(x)
+      x = tf.nn.relu(x)
+      x = self.post2(x)
     tf.summary.histogram('z_e', x)
     return x
 
--- generate.py
+++ generate.py
@@ -14,6 +14,7 @@ def parse_args():
   parser = ArgumentParser()
   parser.add_argument('-restore', help='path to weights')
   parser.add_argument('-audio', help='path to audio')
+  parser.add_argument('-gc', default='accent', choices=['speaker', 'accent'])
   parser.add_argument('-speakers', nargs='+')
   args = parser.parse_args()
   return args
@@ -34,36 +35,39 @@ def gaussian_sample(logits):
 
 def main():
   args = parse_args()
-  batch_size = len(args.speakers)
-  batch_size = 11
+  if args.gc == 'speaker':
+    batch_size = len(args.speakers)
+  else:
+    batch_size = 11
 
   wav, _ = librosa.load(args.audio, sr=parameters.sr)
-  # 512 is the largest dilation rate; change as needed
-  wav = tf.reshape(wav[:wav.shape[0] // 512 * 512], [1, -1, 1])
-  wav = tf.tile(wav, [batch_size, 1, 1])
-
-  sess = tf.Session()
-  wav = sess.run(wav)
+  wav = np.reshape(wav[: wav.shape[0] // 64 * 64], [1, -1, 1])
+  wav = np.reshape(wav, [1, -1, 1])
+  wav = np.tile(wav, [batch_size, 1, 1])
   length = wav.shape[1]
+  print('length:', wav.shape)
 
-  '''
-  speaker_to_int = get_speaker_to_int('data/vctk_info/vctk_speakers.txt')
-  speaker = [[0 for _ in range(109)] for _ in range(len(args.speakers))]
-  num_speakers = 109
-  '''
+  if args.gc == 'speaker':
+    speaker_to_int = get_speaker_to_int('vctk_speakers.txt')
+    # speaker_to_int = get_speaker_to_int('data/vctk_info/vctk_speakers.txt')
+    h = [[speaker_to_int[s]] for s in args.speakers]
+    gc_dim = 109
+  else:
+    h = [[i] for i in range(batch_size)]
+    gc_dim = batch_size
+  print('gc:', np.shape(h))
 
   encoder = Encoder(scope='encoder', latent_dim=parameters.latent_dim, trainable=False)
   vqvae = VQVAE(scope='vqvae', trainable=False)
   condition = vqvae(encoder(wav))
-  print('condition:', condition.shape)
+  print('lc:', condition.shape)
 
-  h = [[i] for i in range(batch_size)]
-  gc_dim = 11
   gc_embedding = tf.get_variable(
       name='decoder/gc_embedding', 
       shape=[gc_dim, parameters.global_embedding_dim])
   h = tf.nn.embedding_lookup(gc_embedding, h)
   h = tf.tile(h, [1, condition.get_shape().as_list()[1], 1])
+  print('gc:', h.shape)
   condition = tf.concat([condition, h], axis=-1)
   print('condition:', condition.shape)
 
@@ -71,7 +75,8 @@ def main():
   input_t = tf.placeholder(tf.float32, [batch_size, 1])
   condition_t = tf.placeholder(tf.float32, [batch_size, condition.shape[-1]])
   init_ops, push_ops, x = decoder.generate(input_t, condition_t, batch_size=batch_size)
-
+  exit()
+  sess = tf.Session()
   saver = tf.train.Saver()
   latest_checkpoint = tf.train.latest_checkpoint(args.restore)
   gs = int(latest_checkpoint.split('-')[-1])
@@ -99,13 +104,14 @@ def main():
     decoded = gaussian_sample(probs)
     to_write[:, i] = decoded
     audio = np.expand_dims(decoded, -1)
-  '''
-  for i, s in enumerate(args.speakers):
-    s = 'no_speaker' if s == 'None' else s
-    wavfile.write(args.restore + '/%d_%s.wav'%(gs, s), 16000, to_write[i])
-  '''
-  for i in range(batch_size):
-    wavfile.write(args.restore + '/%d_accent_%s.wav'%(gs, i), 16000, to_write[i])
+  
+  if args.gc == 'speaker':
+    for i, s in enumerate(args.speakers):
+      s = 'no_speaker' if s == 'None' else s
+      wavfile.write(args.restore + '/%d_%s.wav'%(gs, s), parameters.sr, to_write[i])
+  else:
+    for i in range(batch_size):
+      wavfile.write(args.restore + '/%d_accent_%s.wav'%(gs, i), parameters.sr, to_write[i])
 
 if __name__ == '__main__':
   suppress_tf_warning()
